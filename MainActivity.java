0d4ae4efcf309934fc94a08d5092cdbf9ee4271a
Shriyanshh <74383360+Shriyanshh@users.noreply.github.com>
2021-03-13 03:43:19 -0500

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.flashcardapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -22,6 +22,31 @@ public class MainActivity extends AppCompatActivity {
                 flashcardQuestion.setVisibility(View.INVISIBLE);
             }
         });
+        flashcardAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flashcardAnswer.setVisibility(View.INVISIBLE);
+                flashcardQuestion.setVisibility(View.VISIBLE);
+            }
+        });
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+                MainActivity.this.startActivityForResult(intent, 100);
+            }
+        });
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100) { // this 100 needs to match the 100 we used when we called startActivityForResult!
+            TextView flashcardQuestion = findViewById(R.id.flashcard_question);
+            flashcardQuestion.setText(data.getExtras().getString("new_question"));
+            TextView flashcardAnswer = findViewById(R.id.flashcard_answer);
+            flashcardAnswer.setText(data.getExtras().getString("new_answer"));
+        }
     }
 }
 
